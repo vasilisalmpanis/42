f086f754075b4365415b8fe03c68e91e3b643fc9
Vasileios Almpanis <valmpani@c2s18d119.42wolfsburg.de>
2023-05-11 14:51:05 +0200

    finished strchr

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: valmpani <valmpani@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 08:08:00 by valmpani          #+#    #+#             */
-/*   Updated: 2023/05/11 13:11:32 by valmpani         ###   ########.fr       */
+/*   Updated: 2023/05/11 14:28:04 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,9 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+// size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif

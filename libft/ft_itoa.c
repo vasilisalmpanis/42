6bba65a2ae9c627fb6bbb2308d722a0384714c16
Vasileios Almpanis <valmpani@c2s18d119.42wolfsburg.de>
2023-05-14 11:08:22 +0200

    finished with itoa strting strmapi

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,17 +6,73 @@
 /*   By: valmpani <valmpani@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 13:41:12 by valmpani          #+#    #+#             */
-/*   Updated: 2023/05/13 13:49:49 by valmpani         ###   ########.fr       */
+/*   Updated: 2023/05/13 20:19:44 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char ft_is_pn(int c)
+int	ft_find_length(long c)
+{
+	int	length;
+
+	length = 0;
+	if (c == 0)
+		return (1);
+	while (c != 0)
+	{
+		c = c / 10;
+		length++;
+	}
+	return (length);
+}
+
+int	ft_is_pn(long c)
 {
 	if (c >= 0)
-		return ('');
-	if (c < 0)
-		return ('-');
+		return (1);
+	else
+		return (0);
 }
+
 char	*ft_itoa(int n)
+{
+	long	value;
+	long	number;
+	char	*buffer;
+
+	number = n;
+	value = ft_find_length(n) + 1;
+	if (number < 0)
+	{
+		number *= -1;
+		value++;
+	}
+	buffer = ft_calloc(value, 1);
+	if (!buffer)
+		return (NULL);
+	while (value > 0)
+	{
+		buffer[value - 2] = number % 10 + '0';
+		number = number / 10;
+		value--;
+	}
+	if (n < 0)
+		buffer[0] = '-';
+	return (buffer);
+}
+
+// int	main(void)
+// {
+// 	char *s;
+// 	s = ft_itoa(-1234);
+
+// 	// s = ft_itoa(1);
+
+// 	// s = ft_itoa(-1);
+// 	// s = ft_itoa(42);
+// 	printf("%s", s);
+// 	free(s);
+// 	return 0;
+// }

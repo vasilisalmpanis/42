15316e3b5cdcd030064b2f2ff4e6e9f7b3b4d082
Vasileios Almpanis <valmpani@c2s18d119.42wolfsburg.de>
2023-05-15 17:19:00 +0200

    started bonus finished lstnew lstadd_front lstsize and doing lstlast

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: valmpani <valmpani@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 08:08:00 by valmpani          #+#    #+#             */
-/*   Updated: 2023/05/14 16:36:24 by valmpani         ###   ########.fr       */
+/*   Updated: 2023/05/15 17:17:02 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,16 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 char	**ft_split(char const *s, char c);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif

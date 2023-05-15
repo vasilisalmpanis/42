5acfef53bfe3b139850681ba734c378e5736a6d1
Vasileios Almpanis <valmpani@c2s18d119.42wolfsburg.de>
2023-05-15 18:55:16 +0200

    finished lstdelone

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: valmpani <valmpani@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 08:08:00 by valmpani          #+#    #+#             */
-/*   Updated: 2023/05/15 17:24:57 by valmpani         ###   ########.fr       */
+/*   Updated: 2023/05/15 18:07:35 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif

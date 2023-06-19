ead60d520406eee40c432eeb7d15fe0d8dcef360
valmpani <vasilisalmpanis@gmail.com>
2023-06-19 13:08:56 +0200

    error messages for file fds

--- pipex/errors.c
+++ pipex/errors.c
@@ -1,37 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: valmpani <valmpanis@student.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/19 10:47:12 by valmpani          #+#    #+#             */
-/*   Updated: 2023/06/19 13:06:39 by valmpani         ###   ########.fr       */
+/*   Created: 2023/06/19 12:58:45 by valmpani          #+#    #+#             */
+/*   Updated: 2023/06/19 13:06:31 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
-# include <unistd.h>
-# include <errno.h>
-# include <stdio.h>
-# include <string.h>
-# include "libft/libft.h"
+#include "pipex.h"
 
-typedef struct s_data
+void	error_files(char *object)
 {
-	int		fd[2];
-	int		file[2];
-	pid_t	pid[2];
-	char	**split;
-	char	*cmd;
-	int		argc;
-	char	**argv;
-	char	**env;
-}	t_data;
-
-int		main(int argc, char **argv, char **env);
-int		has_access(t_data object);
-void	error_files(char *object);
-
-#endif
+	ft_printf("Pipex: ");
+	perror(object);
+}
--- pipex/main.c
+++ pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: valmpani <valmpanis@student.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 10:46:50 by valmpani          #+#    #+#             */
-/*   Updated: 2023/06/19 11:54:53 by valmpani         ###   ########.fr       */
+/*   Updated: 2023/06/19 13:06:54 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,11 @@ int	main(int argc, char **argv, char **env)
 	object.argc = argc;
 	object.argv = argv;
 	object.env = env;
-	object.file_fd[0] = has_access(object);
-	if (object.file_fd[0] == -1)
-	{
-		ft_printf("Pipex: ");
-		perror(object.argv[1]);
-	}
-	object.file_fd[1] = open(object.argv[argc - 1], O_WRONLY, O_TRUNC, O_CREAT, 0777);
-	if (object.file_fd[1] == -1)
+	object.file[0] = has_access(object);
+	if (object.file[0] == -1)
+		error_files(object.argv[1]);
+	object.file[1] = open(argv[argc - 1], O_WRONLY | O_TRUNC | O_CREAT, 0777);
+	if (object.file[1] == -1)
+		error_files(object.argv[argc - 1]);
 	return (0);
 }

930e4ea41f85b2bf835f36850a1e6137360f4b11
valmpani <vasilisalmpanis@gmail.com>
2023-07-07 14:49:07 +0200

    began with fractol

--- fractol/mandatory/fractol.h
+++ fractol/mandatory/fractol.h
@@ -6,15 +6,52 @@
 /*   By: valmpani <valmpanis@student.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 12:33:25 by valmpani          #+#    #+#             */
-/*   Updated: 2023/07/04 12:59:09 by valmpani         ###   ########.fr       */
+/*   Updated: 2023/07/07 12:39:58 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
-# include "../libft/libft.h"
+# include <math.h>
 # include "../mlx/mlx.h"
+# include "../libft/libft.h"
 
-int	main(void);
+# define WIDTH 1600
+# define HEIGHT 900
+# define M_I 250
+
+typedef struct s_color
+{
+	int	red;
+	int	green;
+	int	blue;
+}	t_color;
+
+typedef struct s_complex
+{
+	double		min_r;
+	double		max_r;
+	double		min_i;
+	double		max_i;
+	double		factor_r;
+	double		factor_i;
+	double		c_r;
+	double		c_i;
+	double		z_r;
+	double		z_i;
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*img_data;
+	int			bpp;
+	int			size_line;
+	int			endian;
+	t_color		colours[M_I + 1];
+	t_color		color;
+}	t_complex;
+
+t_complex	init_fractol(void);
+void		draw_mandelbrot_set(t_complex f);
+int			main(int argc, char **argv);
 
 #endif
--- fractol/mandatory/main.c
+++ fractol/mandatory/main.c
@@ -6,33 +6,108 @@
 /*   By: valmpani <valmpanis@student.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 12:33:55 by valmpani          #+#    #+#             */
-/*   Updated: 2023/07/04 15:30:57 by valmpani         ###   ########.fr       */
+/*   Updated: 2023/07/07 13:17:52 by valmpani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-
-int	main(void)
+void	no_parameters(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	ft_printf("Available sets:\n");
+	ft_printf("---> Mandelbrot\n");
+	ft_printf("---> Julia\n");
+	exit(0);
+}
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 500, 500, "Fract-ol");
-	for (int i = 0; i < 500; i++)
+void	set_color(t_color *color, int red, int green, int blue)
+{
+	color->red = red;
+	color->green = green;
+	color->blue = blue;
+}
+
+void	draw_mandelbrot_set(t_complex f)
+{
+	double		temp;
+	int			i;
+	int			j;
+	int			iteration;
+
+	j = -1;
+	while (++j < HEIGHT)
 	{
-		for (int x = 0; x < 500; x++)
+		f.c_i = f.max_i - j * f.factor_i;
+		i = -1;
+		while (++i < WIDTH)
 		{
-			if (x % 2 != 0)
-				mlx_pixel_put(mlx, mlx_win, i, x, 0x000000);
-			else if (x % 3 == 0)
-				mlx_pixel_put(mlx, mlx_win, i, x, 0x00FF00);
+			f.c_r = f.min_r + i * f.factor_r;
+			f.z_r = 0;
+			f.z_i = 0;
+			iteration = 0;
+			while (f.z_r * f.z_r + f.z_i * f.z_i <= 4 && iteration < M_I)
+			{
+				temp = f.z_r * f.z_r - f.z_i * f.z_i + f.c_r;
+				f.z_i = 2 * f.z_r * f.z_i + f.c_i;
+				f.z_r = temp;
+				iteration++;
+			}
+			if (iteration < M_I)
+				f.color = f.colours[iteration];
 			else
-				mlx_pixel_put(mlx, mlx_win, i, x, 0xFF0000);
+				set_color(&f.color, 0, 0, 0);
+
+			f.img_data[(j * f.size_line) + (i * 4)] = f.color.blue;
+			f.img_data[(j * f.size_line) + (i * 4) + 1] = f.color.green;
+			f.img_data[(j * f.size_line) + (i * 4) + 2] = f.color.red;
 		}
 	}
-	// mlx_string_put(mlx, mlx_win, 250, 250, 0xFFFFFF, "hello world\n");
-	// mlx_string_put(mlx, mlx_win, 250, 260, 0xFFFFFF, "hello world\n");
-	mlx_loop(mlx);
+	mlx_put_image_to_window(f.mlx, f.win, f.img, 0, 0);
+}
+
+t_complex	init_fractol(void)
+{
+	t_complex	c;
+	int			i;
+	int			red;
+	int			green;
+	int			blue;
+
+	c.mlx = mlx_init();
+	c.win = mlx_new_window(c.mlx, WIDTH, HEIGHT, "Mandelbrot Set");
+	c.min_r = -2.0;
+	c.min_i = -1.0;
+	c.max_r = 1.0;
+	c.max_i = 1.0;
+	c.img = mlx_new_image(c.mlx, WIDTH, HEIGHT);
+	c.img_data = mlx_get_data_addr(c.img, &c.bpp, &c.size_line, &c.endian);
+	c.factor_r = (c.max_r - c.min_r) / (WIDTH - 1);
+	c.factor_i = (c.max_i - c.min_i) / (HEIGHT - 1);
+	i = -1;
+	while (++i < M_I)
+	{
+		red = i % 256;
+		green = (i * 2) % 256;
+		blue = (i * 3) % 256;
+		set_color(&c.colours[i], red, green, blue);
+	}
+	return (c);
+}
+
+int	main(int argc, char **argv)
+{
+	t_complex	f;
+
+	if (argc != 2)
+		no_parameters();
+	if (ft_strncmp(argv[1], "Mandelbrot", 10) == 0)
+	{
+		f = init_fractol();
+		draw_mandelbrot_set(f);
+		mlx_loop(f.mlx);
+		return (0);
+	}
+	else if (ft_strncmp(argv[1], "Julia", 5) == 0)
+		ft_printf("Julia\n");
+	return (0);
 }

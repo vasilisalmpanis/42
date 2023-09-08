834fe5308b0e6fa2baa98492a9c5a761a3506a5a
valmpani <vasilisalmpanis@gmail.com>
2023-09-08 10:41:46 +0200

    finished

---
+++ Philosophers/init_prog.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_prog.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: valmpani <valmpani@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 18:30:06 by valmpani          #+#    #+#             */
+/*   Updated: 2023/09/04 18:30:09 by valmpani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	parse(char **argv, int argc)
+{
+	if (argc != 5 && argc != 6)
+		return (1);
+	if (ft_atoi(argv[1]) < 0)
+		return (1);
+	if (ft_atoi(argv[2]) < 60)
+		return (1);
+	if (ft_atoi(argv[3]) < 60)
+		return (1);
+	if (ft_atoi(argv[4]) < 60)
+		return (1);
+	if (argc == 6 && ft_atoi(argv[5]) < 1)
+		return (1);
+	return (0);
+}
+
+void	init_right_fork(t_prog **prog)
+{
+	t_philo	*philo;
+	t_philo	*next_philo;
+	int		i;
+
+	i = -1;
+	while (++i < (*prog)->phil_num)
+	{
+		philo = &(*prog)->threads[i];
+		next_philo = &(*prog)->threads[(i + 1) % (*prog)->phil_num];
+		philo->right_fork = next_philo->left_fork;
+	}
+}
+
+void	init_threads(t_prog **prog)
+{
+	int	i;
+
+	(*prog)->start_time = get_time();
+	i = -1;
+	while (++i < (*prog)->phil_num)
+	{
+		pthread_create(&(*prog)->philo[i], NULL, routine,
+			(void *)(&(*prog)->threads[i]));
+	}
+}
+
+t_prog	*initialize_prog(int argc, char **argv)
+{
+	t_prog	*prog;
+
+	prog = ft_calloc(1, sizeof(t_prog));
+	if (!prog)
+		return (NULL);
+	prog->phil_num = ft_atoi(argv[1]);
+	prog->time_to_die = ft_atoi(argv[2]);
+	prog->time_to_eat = ft_atoi(argv[3]);
+	prog->time_to_sleep = ft_atoi(argv[4]);
+	prog->philo = malloc(sizeof(pthread_t) * prog->phil_num);
+	if (argc == 6)
+		prog->meals = ft_atoi(argv[5]);
+	else
+		prog->meals = -1;
+	if (pthread_mutex_init(&prog->is_dead, NULL) != 0 || \
+		pthread_mutex_init(&prog->meal_proc, NULL) != 0 || \
+		pthread_mutex_init(&prog->print, NULL) != 0)
+		return (NULL);
+	if (init_philo(&prog, prog->phil_num))
+		return (NULL);
+	return (prog);
+}
+
+int	init_philo(t_prog **prog, int num)
+{
+	int	i;
+
+	(*prog)->threads = malloc(num * sizeof(t_philo));
+	if (!(*prog)->threads)
+		return (1);
+	i = -1;
+	while (++i < num)
+	{
+		(*prog)->threads[i].id = i;
+		(*prog)->threads[i].left_fork = malloc(sizeof(pthread_mutex_t));
+		if (pthread_mutex_init((*prog)->threads[i].left_fork, NULL) != 0)
+			return (1);
+		(*prog)->threads[i].dead = &(*prog)->dead;
+		(*prog)->threads[i].prog = (*prog);
+		(*prog)->threads[i].meals_eatten = 0;
+		(*prog)->threads[i].last_meal = 0;
+	}
+	init_right_fork(prog);
+	(*prog)->start_time = get_time();
+	init_threads(prog);
+	return (0);
+}
---
+++ Philosophers/routine.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: valmpani <valmpani@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 18:37:36 by valmpani          #+#    #+#             */
+/*   Updated: 2023/09/05 18:37:42 by valmpani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	update_meal_time(t_philo *thread)
+{
+	pthread_mutex_lock(&thread->prog->meal_proc);
+	thread->meals_eatten += 1;
+	thread->last_meal = get_time() + thread->prog->time_to_die;
+	pthread_mutex_unlock(&thread->prog->meal_proc);
+}
+
+int	all_full(t_philo *thread)
+{
+	int	result;
+
+	pthread_mutex_lock(&thread->prog->meal_proc);
+	result = thread->prog->all_fed;
+	pthread_mutex_unlock(&thread->prog->meal_proc);
+	return (result);
+}
+
+int	ft_sleep(t_philo *thread)
+{
+	if (philo_died(thread) || all_full(thread))
+		return (1);
+	print_message(thread, SLEEPING);
+	ft_usleep(thread->prog->time_to_sleep);
+	return (0);
+}
+
+int	think(t_philo *thread)
+{
+	if (philo_died(thread) || all_full(thread))
+		return (1);
+	print_message(thread, THINKING);
+	return (0);
+}
+
+void	*routine(void *prog)
+{
+	t_philo	*thread;
+
+	thread = prog;
+	pthread_mutex_lock(&thread->prog->meal_proc);
+	thread->last_meal = get_time() + thread->prog->time_to_die;
+	pthread_mutex_unlock(&thread->prog->meal_proc);
+	while (1)
+	{
+		if (eat(thread))
+			break ;
+		if (ft_sleep(thread))
+			break ;
+		if (think(thread))
+			break ;
+	}
+	return (NULL);
+}

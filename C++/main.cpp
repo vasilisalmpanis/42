772562bd5e33adcde3150478c208c6a53d0e75bf
vasilis <vasilisalmpanis@gmail.com>
2023-10-12 11:26:12 +0200

    cpp00

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <string> // You need to include the <string> header for string data type.
 
-class Employee {
+class AbstractEmployee {
+    virtual void Askforpromotion() = 0;
+};
+
+class Employee:AbstractEmployee {
 private:
     std::string Name;
     std::string Company;
     int Age;
 public:
-    void set_name(std::string name) {
+    void set_name(const std::string& name) {
         Name = name;
     }
     std::string get_name() {
         return Name;
     }
-    void set_company(std::string company) {
+    void set_company(const std::string& company) {
         Company = company;
     }
     std::string get_company() {
@@ -23,7 +27,7 @@ public:
         if (age > 18)
             Age = age;
     }
-    int get_age() {
+    int get_age() const {
         return Age;
     }
     void introduceyourself()
@@ -34,18 +38,54 @@ public:
     }
 
     // You should use std::string instead of just string.
-    Employee(std::string name, std::string company, int age) {
+    Employee(const std::string& name, const std::string& company, int age) : Age() {
         set_name(name);
         set_company(company);
         set_age(age);
     }
+    void Askforpromotion() {
+        if (Age > 30)
+            std::cout << Name << " got promoted!." << std::endl;
+        else
+            std::cout << Name << " Sorry no promotion!." << std::endl;
+
+    }
 };
 
-int main(void)
+class Developer:public Employee {
+public:
+	std::string fpl;
+	Developer(const std::string& name, const std::string& company, int age, const std::string& lang)
+			:Employee(name, company, age)
+	{
+		fpl = lang;
+	}
+	void    fix_bug() {
+		std::cout << get_name() << " fixed bug using " << fpl << std::endl;
+	}
+};
+
+class Teacher: public Employee{
+public:
+	std::string sub;
+	void preparedlesson() {
+		std::cout << get_name() << " is preparin " << sub << "lesson" << std::endl;
+	}
+	Teacher(const std::string& name, const std::string& company, int age, const std::string& subj)
+		:Employee(name, company, age)
+	{
+		sub = subj;
+	}
+};
+
+int main()
 {
     // You can create an instance of the Employee class like this.
     Employee employee1("vasilis", "google", 5);
-    employee1.introduceyourself();
-    employee1.set_age(30);
-    employee1.introduceyourself();
+	Developer de = Developer("Vasilis", "google", 32, "c++");
+	de.fix_bug();
+	de.Askforpromotion();
+	Teacher hello = Teacher("vasi", "hello", 35, "history");
+	hello.preparedlesson();
+	hello.Askforpromotion();
 }

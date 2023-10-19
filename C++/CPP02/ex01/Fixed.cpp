deadb0ad8108b9df94527b279a111125c29c1cbe
valmpani <vasilisalmpanis@gmail.com>
2023-10-19 11:57:20 +0200

    cpp02 ex01

--- C++/CPP02/ex01/Fixed.cpp
+++ C++/CPP02/ex01/Fixed.cpp
@@ -10,24 +10,45 @@ Fixed::Fixed() : _fixedPoint(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::~Fixed() {
-	std::cout << "Default destructor called" << std::endl;
-}
-
 Fixed::Fixed(const Fixed &obj) {
 	std::cout << "Copy constructor called" << std::endl;
 	if (this != &obj)
 		*this = obj;
 }
 
+Fixed::Fixed(const int num) : _fixedPoint(num << this->_fractionalBits)  {
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float num) : _fixedPoint((int)roundf((num * (int)(1 << this->_fractionalBits))))  {
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
 Fixed &Fixed::operator=(const Fixed &obj) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &obj)
-		this->_fixedPoint = obj.getRawBits();
+		this->_fixedPoint = obj._fixedPoint;
 	return (*this);
 }
 
 int Fixed::getRawBits( void ) const{
 	std::cout << "getRawBits member function called" << std::endl;
 	return (_fixedPoint);
+}
+
+int Fixed::toInt() const {
+	return ((int)(_fixedPoint >> this->_fractionalBits));
+}
+
+float Fixed::toFloat() const{
+	return ((float)_fixedPoint / (float)(1 << _fractionalBits));
+}
+
+std::ostream & operator << (std::ostream &out, const Fixed &obj){
+	out << obj.toFloat();
+	return out;
 }
--- C++/CPP02/ex01/Fixed.h
+++ C++/CPP02/ex01/Fixed.h
@@ -5,19 +5,26 @@
 #ifndef Fixed_HPP
 #define Fixed_HPP
 #include <iostream>
+#include <cmath>
 
 class Fixed {
 private:
-	int	_fixedPoint;
-	static const int _fractionalBits;
+	int					_fixedPoint;
+	static const int	_fractionalBits;
 public:
 	Fixed();
 	~Fixed();
 	Fixed(Fixed const &bj);
+	Fixed(const int num);
+	Fixed(const float num);
 	Fixed &operator= (const Fixed &obj);
-	int getRawBits( void ) const;
-	void setRawBits( int const raw );
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) 	const;
+	int		toInt( void ) const;
+	// friend std::ostream &operator << (std::ostream &out , const Fixed &obj);
 };
 
+std::ostream & operator << (std::ostream &out, const Fixed &obj);
 
 #endif

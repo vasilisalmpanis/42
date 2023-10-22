60107b983bd17a5f39e87874ac0177aa4ac79ff6
valmpani <vasilisalmpanis@gmail.com>
2023-10-22 16:56:06 +0200

    cpp01 fixed

--- C++/CPP01/ex04/main.cpp
+++ C++/CPP01/ex04/main.cpp
@@ -13,6 +13,16 @@
 #include <iostream>
 #include <fstream>
 
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		++i;
+	return (i);
+}
+
 void replace_content(std::string &output, std::string oldString, std::string newString)
 {
 	size_t index;
@@ -22,7 +32,7 @@ void replace_content(std::string &output, std::string oldString, std::string new
 	{
 		output.erase(index, oldString.length());
 		output.insert(index, newString);
-		index = output.find(oldString);
+		index = output.find(oldString, index + newString.length());
 	}
 }
 
@@ -32,15 +42,17 @@ int main(int argc, char **argv)
 	std::fstream re;
 	std::string line;
 	std::string output;
-	if (argc != 4)
-	{
+	if (argc != 4) {
 		std::cout << "Please provide 3 arguments." << std::endl;
+		return (1);
+	}
+	else if (ft_strlen(argv[2]) == 0){
+		std::cout << "Please provide a valid string to replace." << std::endl;
 		return (1);
 	}
 	std::fstream file("replacement.txt", std::ios::out);
 	fs.open(argv[1]);
-	if (!fs.good())
-	{
+	if (!fs.good()) {
 		std::cout << "Please provide a valid filename." << std::endl;
 		return (1);
 	}
@@ -52,7 +64,7 @@ int main(int argc, char **argv)
 		output.append("\n");
 	}
 	replace_content(output, argv[2], argv[3]);
-	output.pop_back();
+	output.erase(output.length() - 1, 1);
 	re << output;
 	re.close();
 	return (0);

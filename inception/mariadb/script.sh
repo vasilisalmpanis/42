b1ba0d7354c2336bb5e71cb4f904081dec69ab58
Vasileios Almpanis <valmpani@c2s9d39.42wolfsburg.de>
2023-11-09 15:46:14 +0100

    inception

--- inception/mariadb/script.sh
+++ inception/mariadb/script.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+cat << EOF > /etc/mysql/my.cnf
+[mysqld]
+datadir = /var/lib/mysql
+bind-address = 0.0.0.0
+user = root
+port = 3306
+general_log = 1
+general_log_file = /var/lib/mysql/error.log
+EOF
+chown -R mysql:mysql /var/lib/mysql
+mkdir /run/mysqld
+chown -R mysql:mysql /run/mysqld
+touch /var/lib/mysql/initialized
+mariadb-install-db --user=mysql --data-dir=/var/lib/mysql
 /etc/init.d/mariadb start
 mariadb -e "CREATE DATABASE IF NOT EXISTS $DBNAME;"
-mariadb -e "CREATE USER IF NOT EXISTS '$DBUSER'@'$DBHOST' IDENTIFIED BY '$DBPASS';"
-mariadb -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'$DBHOST';"
+mariadb -e "CREATE USER IF NOT EXISTS '$DBUSER'@'$WPHOST.$WPHOST' IDENTIFIED BY '$DBPASS';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'$WPHOST.$WPHOST' IDENTIFIED BY '$DBPASS';"
 mariadb -e "FLUSH PRIVILEGES;"
-mariadb -e "SHOW DATABASES;"
-
-/bin/bash
+/etc/init.d/mariadb stop
+mysqld
---
+++ inception/wordpress/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir /run/php
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+cd /var/www/html/
+wp core download --allow-root
+wp core config --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=$DBHOST --allow-root
+chown -R www-data:www-data /var/www/html
+chmod 600 wp-config.php
+wp core install --allow-root --url=http://$HOST:8080/ --title="My site" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
+wp --allow-root user create $WP_USER  $WP_EMAIL --role=author
+wp --allow-root user update $WP_USER --user_pass=$WP_PASS --display_name="Ron_loves_harry"
+wp --allow-root theme install Blocksy
+wp --allow-root theme activate Blocksy
+php-fpm7.4 -F

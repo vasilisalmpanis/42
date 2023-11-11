c44ddc6e3a5eb9f9dbc807635d90048f2ffd9196
valmpani <vasilisalmpanis@gmail.com>
2023-11-11 13:39:04 +0100

    inception with 1st bonus adminer working and python blog on localhost subdomain

--- inception/bonus/adminer/script.sh
+++ inception/bonus/adminer/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /var/www/html/
-wget https://www.adminer.org/latest.php -O adminer.php
-chown -R www-data:www-data adminer.php
-chmod 755 adminer.php
-php -S 0.0.0.0:8080
+useradd adminer
+mkdir -p /usr/share/adminer/
+chown  -R adminer:adminer /usr/share/adminer/
+cd /usr/share/adminer
+php -S 0.0.0.0:81 /etc/adminer/conf.php

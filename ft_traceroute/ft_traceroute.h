0f305f4fd09d44f4f86529a24306f4f837b58c56
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2024-11-05 11:06:29 +0100

    ft-traceroute: initial step
    adds overall opts struct, creates raw socket and binds to interface
    displays error messages in case something goes wrong.
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- ft_traceroute/ft_traceroute.h
+++ ft_traceroute/ft_traceroute.h
@@ -2,15 +2,25 @@
 #define FT_TRACEROUTE
 
 #include <argp.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 
 #include "defines.h"
 #include "libft/libft.h"
-#include "stdbool.h"
+struct sock {
+    int fd;
+    int type;
+};
 
 struct opts {
     int max_ttl;
+    int current_ttl;
     bool verbose;
+    struct sock socket;
+    char *intf;
 };
 #endif  // !FT_TRACEROUTE
--- ft_traceroute/main.c
+++ ft_traceroute/main.c
@@ -1,4 +1,4 @@
-#include <stdbool.h>
+#include <argp.h>
 #include <stdio.h>
 
 #include "ft_traceroute.h"
@@ -8,8 +8,7 @@ struct opts opts;
 static const struct argp_option options[] = {
     {"verbose", 'V', 0, 0, "Print version info and exit", 0},
     {"interface", 'i', "device", 0, "Specify a network interface to operate with", 0},
-    {"max-hops", 'm', "NUM", 0, "Set the max number of hops (max TTL to be reached). Default is 30",
-     0},
+    {"max-hops", 'm', "NUM", 0, "Set the max number of hops (max TTL to be reached). Default is 30", 0},
     {0}};
 
 void print_opts()
@@ -27,24 +26,51 @@ int parse_opt(int key, char *arg, struct argp_state *state)
     switch (key) {
         case 'm':
             opts.max_ttl = atoi(arg);
-            return 0;
+            break;
         case 'V':
-	    opts.verbose = true;
-	    return 0;
+            opts.verbose = true;
+            break;
+        case 'i':
+            if (ft_strlen(arg) >= IFNAMSIZ) {
+                printf("Please provide a valid interface name\n");
+                return ARGP_ERR_UNKNOWN;
+            }
+            opts.intf = arg;
+            break;
         default:
             return ARGP_ERR_UNKNOWN;
     }
+    return 0;
 }
 
+void set_up_opts()
+{
+    opts.max_ttl     = 30;
+    opts.current_ttl = 1;
+    opts.verbose     = false;
+    opts.socket.fd   = 0;
+    opts.socket.type = 0;
+    opts.intf        = NULL;
+}
 int main(int argc, char *argv[])
 {
     struct argp argp = {options, parse_opt, "", "", NULL, NULL, NULL};
+    struct ifreq ifr;
+    set_up_opts();
     argp_parse(&argp, argc, argv, 0, 0, 0);
-    // create socket
-    // bind to specific interface if provifed by the user.
+    opts.socket.fd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    if (opts.socket.fd == -1)
+        printf("opening socket failed\n");
+    if (opts.intf) {
+        snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "%s", opts.intf);
+        if (setsockopt(opts.socket.fd, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
+            printf("could not bind to interface\n");
+        }
+    }
     // start sending ICMP packets with ttl starting from 1 and increasing to max. default30
     // do reverse dns resolution and display the route.
     // exit
     print_opts();
+    close(opts.socket.fd);
     return 0;
 }

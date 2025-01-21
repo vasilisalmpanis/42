a894e78a4fae6a15e09d2a5695d2e173b000fac2
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-01-21 11:07:36 +0100

    ft_ping: checksum docs to better understand the algorithm
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- ft_ping/checksum.c
+++ ft_ping/checksum.c
@@ -13,7 +13,7 @@
  */
 unsigned short icmp_checksum(void *packet, size_t length)
 {
-    uint32_t sum    = 0;
+    uint32_t sum    = 0; /* 32 bit ud to save the carry */
     uint32_t answer = 0;
     uint16_t *ptr   = (unsigned short *)packet;
     while (length > 1) {
@@ -22,8 +22,8 @@ unsigned short icmp_checksum(void *packet, size_t length)
     }
     if (length == 1)
         sum += *(uint8_t *)ptr;
-    sum = (sum >> 16) + (sum & 0xffff);
-    sum += (sum >> 16);
-    answer = ~sum;
+    sum = (sum >> 16) + (sum & 0xffff); /* take the first 2 bytes and add the carry */
+    sum += (sum >> 16); 		/* this is probably zero since we already took the carry */
+    answer = ~sum; 			/* Ones compliment by bitwise NOT */
     return answer;
 }

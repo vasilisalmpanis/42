e2cd7224089b6a72093fe3b27c14633834138dd6
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-02-21 12:43:39 +0100

    rustlings: enums
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- rustlings/exercises/08_enums/enums1.rs
+++ rustlings/exercises/08_enums/enums1.rs
@@ -1,6 +1,10 @@
 #[derive(Debug)]
 enum Message {
-    // TODO: Define a few types of messages as used below.
+    Resize = 0,
+    Move = 1,
+    Echo = 2,
+    ChangeColor = 3,
+    Quit = 4,
 }
 
 fn main() {
